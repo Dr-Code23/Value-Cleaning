f29408a6bec39578348cd0ff52f40e69003417c3
Arafat <mohammed.poop444@gmail.com>
2023-04-15 08:52:32 +0200

    update

--- Modules/Auth/Transformers/UserResource.php
+++ Modules/Auth/Transformers/UserResource.php
@@ -29,6 +29,8 @@ class UserResource extends JsonResource
             'count' => User::count(),
             'type' => $this->type,
             'companyId' => $this->companyId ?? 0,
+            'permission' => $this->getPermissionsViaRoles() ?? []
+
 
 
         ];
