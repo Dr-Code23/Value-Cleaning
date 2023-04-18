af89e4c228c27bc89214e06bcd332201b167b3ad
Arafat <mohammed.poop444@gmail.com>
2023-04-19 01:32:01 +0200

    update

--- Modules/Order/Repositories/Repository/OrderAdminRepository.php
+++ Modules/Order/Repositories/Repository/OrderAdminRepository.php
@@ -3,12 +3,18 @@
 namespace Modules\Order\Repositories\Repository;
 
 
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
+use Modules\Category\Entities\Category;
 use Modules\Order\Entities\Order;
 use Modules\Order\Repositories\Interfaces\OrderAdminRepositoryInterface;
 use Modules\Order\Transformers\OrderAdminIndexResource;
 use Modules\Order\Transformers\OrderAdminResource;
+use Modules\Service\Entities\Service;
+use Modules\Service\Entities\SubService;
+use Modules\Worker\Entities\Worker;
+use Spatie\Permission\Models\Role;
 
 class OrderAdminRepository implements OrderAdminRepositoryInterface
 {
@@ -16,13 +22,25 @@ class OrderAdminRepository implements OrderAdminRepositoryInterface
      * @var Order
      */
     private $orderModel;
+    private Service $serviceModel;
+    private Category $categoryMpdel;
+    private SubService $subServiceModel;
+    private User $userModel;
+    private Worker $workerModel;
+    private Role $roleModel;
 
     /**
      * @param Order $order
      */
-    public function __construct(Order $order)
+    public function __construct(Order $order, Service $service, SubService $subService, Category $category, Worker $worker, User $user, Role $role)
     {
         $this->orderModel = $order;
+        $this->serviceModel = $service;
+        $this->categoryMpdel = $category;
+        $this->subServiceModel = $subService;
+        $this->userModel = $user;
+        $this->workerModel = $worker;
+        $this->roleModel = $role;
     }
 
     /**
@@ -143,27 +161,46 @@ class OrderAdminRepository implements OrderAdminRepositoryInterface
      */
     public function home()
     {
+        $service = $this->serviceModel->query()->count();
 
-        $toDay = Order::query()->whereDate('created_at', Carbon::today())->count();
-        $toMonth = Order::query()
+        $category = $this->categoryMpdel->query()->count();
+
+        $subService = $this->subServiceModel->query()->count();
+
+        $user = $this->userModel->query()->where('type', 'user')->count();
+        $company = $this->userModel->query()->where('type', 'company')->count();
+        $employee = $this->userModel
+            ->query()
+            ->where('type', 'admin')
+            ->whereDoesntHave('roles', function ($query) {
+                $query->where('name', 'admin');
+            })->count();
+
+        $worker = $this->workerModel->query()->count();
+
+        $role = $this->roleModel->query()->count();
+
+
+        $toDay = $this->orderModel->query()->whereDate('created_at', Carbon::today())->count();
+        $toMonth = $this->orderModel->query()
             ->whereDate('created_at', '>=', Carbon::now()->startOfMonth())
             ->whereDate('created_at', '<=', Carbon::now()->endOfMonth())
             ->count();
-        $toYear = Order::query()->whereBetween('created_at', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])->count();
-        $total_price_atYear = Order::
+        $toYear = $this->orderModel->query()->whereBetween('created_at', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])->count();
+        $total_price_atYear = $this->orderModel->
         query()
             ->where('status', 'finished')
             ->whereBetween('created_at', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])
             ->sum('total_price');
-        $total_price = Order::query()->where('status', 'finished')
+        $total_price = $this->orderModel->query()->where('status', 'finished')
             ->whereDate('created_at', '>=', Carbon::now()->startOfMonth())
             ->whereDate('created_at', '<=', Carbon::now()->endOfMonth())
             ->sum('total_price');
-        $all_total_price = Order::query()->where('status', 'finished')->sum('total_price');
-        $finished = Order::query()->where('status', 'finished')->count();
+        $all_total_price = $this->orderModel->query()->where('status', 'finished')->sum('total_price');
+        $finished = $this->orderModel->query()->where('status', 'finished')->count();
 
-        $cancel = Order::query()->where('status', 'canceled')->count();
-        $process = Order::query()->where('status', 'processing')->count();
+        $cancel = $this->orderModel->query()->where('status', 'canceled')->count();
+        $process = $this->orderModel->query()->where('status', 'processing')->count();
 
         return response()->json([
             'statusCode' => 200,
@@ -176,7 +213,16 @@ class OrderAdminRepository implements OrderAdminRepositoryInterface
             'toyear' => $toYear,
             'finished' => $finished,
             'cancel' => $cancel,
-            'processing' => $process
+            'processing' => $process,
+            'service' => $service,
+            'category' => $category,
+            'subService' => $subService,
+            'user' => $user,
+            'company' => $company,
+            'employee' => $employee,
+            'worker' => $worker,
+            'role' => $role,
+
         ]);
     }
 
